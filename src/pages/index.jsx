18b44946aa7aa41aa16621a76e52b2aa21e3554d
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-03 21:47:50 -0300

    Add AboutMe Section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
-import CreditCardComponent from '@/components/3D/CreditCard'
-import EstruturaPagina from '../components/EstruturaPagina'
-import Intro from '@/components/Intro'
+import { EstruturaPagina, Intro, AboutMe } from "@/components/Home";
 
 export default function Home() {
   return (
     <EstruturaPagina>
       <Intro/>
+      <AboutMe/>
       <span>Tragetoria</span>
       <span>Dados</span>
       <span>Tecnologias</span>
