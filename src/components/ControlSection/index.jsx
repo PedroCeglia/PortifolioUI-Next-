fa485c75891d4e004aacf443866d3ee1280e00d1
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-05 12:45:09 -0300

    Create a Control Scetion to UI Effecs

---
+++ src/components/ControlSection/index.jsx
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+import { staggerContainer } from "../../constants/motion";
+
+export default function ControlSection (Component, idName){
+  return function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: true, amount: 0.25 }}
+        style={{width:"100%", height:"100%"}}
+      >
+        <div  id={idName}></div>
+        <Component />
+      </motion.section>
+    );
+  };    
+}
