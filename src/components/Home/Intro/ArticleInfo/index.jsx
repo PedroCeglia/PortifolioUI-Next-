09c1953e6c2d1a18d253c2f0cbfea472db002105
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-05 12:50:22 -0300

    Create the ArticleInfo component

---
+++ src/components/Home/Intro/ArticleInfo/index.jsx
@@ -0,0 +1,32 @@
+import { IntroArticleInfoStyle } from "./style"
+import Desktop3DComponent from "@/components/3D/Desktop3DComponent"
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+import { fadeIn } from "@/constants/motion"
+
+export default function ArticleInfo(){
+    const tiltDefaultOptions = {
+        max:35,     // max tilt rotation (degrees)
+        scale:1.01,    // 2 = 200%, 1.5 = 150%, etc..
+        speed:1000   // Speed of the enter/exit transition
+    }
+    const motionVariants = fadeIn("right", "spring", 1 * 0.5, 0.75)
+    return(  
+        <IntroArticleInfoStyle>
+            <Tilt options={tiltDefaultOptions} className="tilt-container">
+                <motion.div className="tilt-content" variants={motionVariants}>           
+                    <h1><span>Olá, eu sou Pedro Ceglia,</span> um desenvolverdor Web!</h1>
+                    <h2><img src="assets/icons/mapa.png"/> Rio de Janeiro, RJ, Brasil <img src="assets/icons/brasil.png"/></h2>
+                    <div className="buttons-area">
+                        <a href="https://github.com/PedroCeglia" target={"_blank"}><img src='assets/icons/github.png'/>Github</a>
+                        <a href="https://www.linkedin.com/in/pedro-ceglia-a914941b7/" target={"_blank"}><img src='assets/icons/linkedin.png'/>Linkedin</a>                    
+                    </div>    
+                    <div className="hasntSize">
+                        <Desktop3DComponent/>
+                    </div>
+                </motion.div>
+            </Tilt>
+
+        </IntroArticleInfoStyle>
+    )
+}
--- src/components/Home/Intro/index.jsx
+++ src/components/Home/Intro/index.jsx
@@ -1,24 +1,13 @@
-import { IntroContainerStyle, IntroArticleInfoStyle } from "./style";
+import { IntroContainerStyle } from "./style";
+import ArticleInfo from "./ArticleInfo";
 import Desktop3DComponent from "../../3D/Desktop3DComponent";
 import ChangeSection from '@/components/ChangeSection';
 
 export default function Intro(){
     return(
-        <IntroContainerStyle id="about">
+        <IntroContainerStyle>
             <div className="content">
-                <IntroArticleInfoStyle>
-                    <div className="content">
-                        <h1><span>Olá, eu sou Pedro Ceglia,</span> um desenvolverdor Web!</h1>
-                        <h2><img src="assets/icons/mapa.png"/> Rio de Janeiro, RJ, Brasil <img src="assets/icons/brasil.png"/></h2>
-                        <div className="buttons-area">
-                            <a href="https://github.com/PedroCeglia" target={"_blank"}><img src='assets/icons/github.png'/>Github</a>
-                            <a href="https://www.linkedin.com/in/pedro-ceglia-a914941b7/" target={"_blank"}><img src='assets/icons/linkedin.png'/>Linkedin</a>                    
-                        </div>    
-                        <div className="hasntSize">
-                            <Desktop3DComponent/>
-                        </div>
-                    </div>
-                </IntroArticleInfoStyle>
+                <ArticleInfo/>
                 <div className="hasSize">
                     <Desktop3DComponent/>
                 </div>
