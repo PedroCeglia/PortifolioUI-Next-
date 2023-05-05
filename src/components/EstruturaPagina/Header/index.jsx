d036ffc203eb122a981228aca33241286a9b690f
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-05 12:43:21 -0300

    Verify the scroll and Change the Header Background

--- src/components/EstruturaPagina/Header/index.jsx
+++ src/components/EstruturaPagina/Header/index.jsx
@@ -4,17 +4,29 @@ import Image from "next/image"
 import { navLinksList } from "@/constants"
 import toggleOpen from "../../../../public/assets/icons/menu.png"
 import toggleClose from "../../../../public/assets/icons/close.png"
+import style from "@/styles"
 
 
-export default function Header(){
+export default function Header({scrollPosition}){
     const [toggleMenu, setToggleMenu] = useState("isClose")
-
     function handleToggleMenuButton(nameClass){
         setToggleMenu(nameClass)
     }
 
+    const [headerBackground, setHeaderBackGround] = useState("transparent")
+    function verifyHeaderPosition(){
+        if(scrollPosition>50){
+            setHeaderBackGround(style.theme.rgbaFunctions.getColor1RGBA(.9))
+        } else{
+            setHeaderBackGround("transparent")
+        }
+    }
+    useEffect(()=>{
+        verifyHeaderPosition()
+    },[scrollPosition])
+
     return(
-        <HeaderStyle>
+        <HeaderStyle style={{backgroundColor:`${headerBackground}`}}>
             <span>PedroCeglia | Developer UI</span>
             <HeaderNavStyle>
                 <div className="hasSize">
@@ -42,7 +54,6 @@ export default function Header(){
                     <Image className="imgToggle closeImage" src={toggleClose} alt="Menu Close Icon" onClick={()=>handleToggleMenuButton("isClose")}/>
                 </div>
             </HeaderNavStyle>
-
         </HeaderStyle>
     )
 }
