3fcedeec042b77ef5b6117ce20411cae9d82b6a7
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-05 12:48:08 -0300

    Change CSS

--- src/components/Home/AboutMe/InfoCard/style.js
+++ src/components/Home/AboutMe/InfoCard/style.js
@@ -4,10 +4,8 @@ import style from "@/styles";
 export const InfoCardStyle = styled.div`
     min-width: 270px;
     width: 100%;
-    max-width: 500px;
-    min-height: 300px;
-
-    background-color: ${style.theme.rgbaFunctions.getColor1RGBA(.5)};
+    max-width: 250px;
+    margin: 20px;
 
     .tilt-container{
         width: 100%;
@@ -16,11 +14,36 @@ export const InfoCardStyle = styled.div`
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        background-color: ${style.theme.rgbaFunctions.getColor1RGBA(.5)};
+        border-radius: 20px;
     }
-    .tilt-content{}
+    .tilt-content{
+        height: 100%;
+        width: 100%;
+        min-height: 300px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        
+        background: ${style.theme.createSimpleGradient(style.theme.rgbaFunctions.getColor1RGBA(.6), style.theme.rgbaFunctions.getLightColorRGBA(.1))};
+        transition: all .4s;
+        border-radius: 20px;
+
+    }
+    .tilt-content:hover{
+        background: ${style.theme.createSimpleGradient(style.theme.rgbaFunctions.getColor1RGBA(.6), style.theme.rgbaFunctions.getLightColorRGBA(.2))};
+    }
     img{
-        width: 40px;
-        height: 40px;
+        margin: 20px;
+        width: 50px;
+        height: 50px;
         object-fit: cover;
+    }
+    h3{
+        font-size: ${style.fonts.fontsSize.big};
+        font-family: ${style.fonts.fontsFamily.brunoAceSc};
+        color: ${style.theme.color.c1};
     }
 `
