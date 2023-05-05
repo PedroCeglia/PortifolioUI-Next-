ce3cb6ee1477258bfd624d9cb14f6afb63e342fd
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-05 12:47:05 -0300

    Change the CSS

--- src/components/Home/AboutMe/style.js
+++ src/components/Home/AboutMe/style.js
@@ -3,9 +3,8 @@ import style from "@/styles";
 
 export const AboutContainerStyle = styled.section`
     width: 100%;
-    padding: 50px 0;
+    padding: 100px 0 50px;
     min-height: 95vh;
-    padding-top: 90px;
 
     position: relative;
     display: flex;
@@ -14,33 +13,49 @@ export const AboutContainerStyle = styled.section`
     align-items: center;
 
     background-color: ${style.theme.rgbaFunctions.getColor2RGBA(.8)};
-    background-repeat: no-repeat; 
-    background-size: cover;
-
     color: ${style.theme.color.c1};
     font-family:${style.fonts.fontsFamily.brunoAceSc};
 
     .content{
-        width: 100%;
+        width: 95%;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items:center;
+        font-size: ${style.fonts.fontsSize.normal};
+        font-family: ${style.fonts.fontsFamily.cinzel};
     }
-
-    @media all and (max-width:1100px){
-        .hasSize{
-            display: none;
-        }
-        .content{
-            flex-direction: column-reverse;
-        }
+    .content .section-head{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
     }
-    @media all and (min-width:1100px){
-        .hasSize{
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
+    .content .section-head p{
+        font-family: ${style.fonts.fontsFamily.rajdhani};
+        font-size: ${style.fonts.fontsSize.big};
+        color: ${style.theme.rgbaFunctions.getLightColorRGBA(.8)};
+    }
+    .content .section-head h2{
+        font-family: ${style.fonts.fontsFamily.cinzel};
+        font-size: ${style.fonts.fontsSize.biggest};
+        margin: 5px 0 20px;
+    }
+    .content .description{
+        width: 70%;
+        align-self: flex-start;
+        max-width: 800px;
+        min-width: 280px;
+        font-family: ${style.fonts.fontsFamily.brunoAceSc};
+        font-size: ${style.fonts.fontsSize.big};
+    }
+    .cards-container{
+        margin-top: 40px;
+        width: 90%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: center;
     }
 `
