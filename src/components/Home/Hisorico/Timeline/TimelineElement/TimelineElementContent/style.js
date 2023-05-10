9c617a8493fed6335638a8d129a201fd1e951390
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-10 01:45:27 -0300

    Create Content CSS

--- src/components/Home/Hisorico/Timeline/TimelineElement/TimelineElementContent/style.js
+++ src/components/Home/Hisorico/Timeline/TimelineElement/TimelineElementContent/style.js
@@ -3,9 +3,35 @@ import style from "@/styles";
 
 export const TimelineElementContentStyle = styled.div`
     width: 100%;
-    height:100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
+
+    div{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+
+        font-size: ${style.fonts.fontsSize.littleBig};
+    }
+    div h3{
+        font-size: ${style.fonts.fontsSize.littleBig};
+        margin: 0;
+    }
+    div p{
+        margin: 0;
+        font-size: ${style.fonts.fontsSize.littleSmall};
+    }
+
+    ul{
+        padding: 20px;
+    }
+    ul li{
+        font-size: ${style.fonts.fontsSize.normal};
+        margin: 10px;
+    }
 `
