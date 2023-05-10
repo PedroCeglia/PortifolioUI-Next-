5b0cc95afdf25921c9b9a4fff626f5dc7c062c38
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-10 01:45:13 -0300

    Add A ControlSection with the slug equals "tech"

--- src/components/Home/Hisorico/index.jsx
+++ src/components/Home/Hisorico/index.jsx
@@ -16,6 +16,7 @@ function HistoricoSection(){
                 description={initialHeaderSectionConfigs.description}
             />
             <Timeline/>
+            <ControlSection slug="tech"/>
         </HistoricoSectionStyle>
     )
 }
