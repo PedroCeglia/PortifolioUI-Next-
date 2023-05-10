39112933e316f43d9f946911edecaf41611aff4d
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-10 01:17:36 -0300

    Create "Historico" Section

---
+++ src/components/Home/Hisorico/index.jsx
@@ -0,0 +1,23 @@
+import { ControlSection, HeaderSection } from "@/components/EstruturaSection";
+import { HistoricoSectionStyle } from "./style";
+import Timeline from "./Timeline";
+
+function HistoricoSection(){
+    const initialHeaderSectionConfigs = {
+        preTitle:"Web Developer | Fullstack Developer |  UI/UX ",
+        title:"Experiências",
+        description:"Ao longo dos meus estudos, estudei diversas tecnologias para diversas area do desenvolvimento de software. Entre elas as que mais me destaquei foram as tecnologias para desenvolvimento Web"
+    }
+    return(
+        <HistoricoSectionStyle>
+            <HeaderSection
+                preTitle={initialHeaderSectionConfigs.preTitle}
+                title={initialHeaderSectionConfigs.title}
+                description={initialHeaderSectionConfigs.description}
+            />
+            <Timeline/>
+        </HistoricoSectionStyle>
+    )
+}
+
+export default ControlSection(HistoricoSection, "historico")
---
+++ src/components/Home/Hisorico/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HistoricoSectionStyle = styled.section`
+    width: 100%;
+    height: 100%;
+    padding: 80px 0 50px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+`
