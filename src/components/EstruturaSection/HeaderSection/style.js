41f935432470f5028dce3de12217fdd6133a5406
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-10 01:14:23 -0300

    Criando CSS

---
+++ src/components/EstruturaSection/HeaderSection/style.js
@@ -0,0 +1,36 @@
+import style from "@/styles";
+import styled from "styled-components";
+
+export const HeaderSectionStyle = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    margin: 30px 0;
+    display: block;
+
+    .section-head{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+    .section-head p{
+        font-family: ${style.fonts.fontsFamily.rajdhani};
+        font-size: ${style.fonts.fontsSize.big};
+        color: ${style.theme.rgbaFunctions.getLightColorRGBA(.8)};
+    }
+    .section-head h2{
+        font-family: ${style.fonts.fontsFamily.montserrat};
+        font-size: ${style.fonts.fontsSize.biggest};
+        margin: 5px 0 20px;
+    }
+    .description{
+        width: 70%;
+        align-self: flex-start;
+        max-width: 800px;
+        min-width: 280px;
+        font-family: ${style.fonts.fontsFamily.montserrat};
+        font-size: ${style.fonts.fontsSize.big};
+    }
+    `
