6e8b89d2f48ac42d5f62b34cf3e024354bd9749b
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-10 01:21:34 -0300

    Create "Constants" index file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,269 @@
+const assetsImage = {
+  backend:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fbackend.png?alt=media&token=8cbb68a9-49c3-4759-a956-c7377605cdd3",
+  creator:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fcreator.png?alt=media&token=b8b56cbb-4c31-4145-ad88-1a0e34ebe31d",
+  web:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fweb.png?alt=media&token=96b51c0d-9ab7-4caa-8155-c9b8d42aaaac",
+  mobile:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fmobile.png?alt=media&token=5ed642e9-3722-4bbb-91c3-816fab679356",
+  tech:{
+    javascript:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fjavascript.png?alt=media&token=7a6dc613-6c50-4078-a268-78b214ae1ad5",
+    typescript:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Ftypescript.png?alt=media&token=9619f0b9-13d0-4464-a9bc-50d9940a9d89",
+    html:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fhtml.png?alt=media&token=d6d0bf2f-572e-4deb-9afb-4f81b42f2a33",
+    css:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fcss.png?alt=media&token=2f149128-892c-4250-8287-47595c1f5a27",
+    reactjs:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Freactjs.png?alt=media&token=aa1b51e7-e7f3-444d-b661-db54c2ceb2d3",
+    redux:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fredux.png?alt=media&token=ac1b00f8-1a63-4ea9-87f3-1d5e460894c5",
+    tailwind:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Ftailwind.png?alt=media&token=45b5fd4f-9b57-43d2-a600-2f41125fc8eb",
+    nodejs:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fnodejs.png?alt=media&token=c4d163f4-09e1-46c2-b9cf-2a2279af26ca",
+    mongoDb:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fmongodb.png?alt=media&token=e45f68db-68cb-4d47-b4ae-42c0c36cce93",
+    git:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fgithub.png?alt=media&token=00f6fdd8-84ae-4952-bfbb-ffb8c3424a57",
+    figma:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Ffigma.png?alt=media&token=7cc057dd-9eba-4157-935d-2d27b84d5ad6",
+    docker:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fdocker.png?alt=media&token=cd709e0e-397a-4452-b975-6ad059d4363d",
+    threejs:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftech%2Fthreejs.svg?alt=media&token=00adacdc-72fa-443d-9e3c-e89d36e5f8ef"
+  },
+  company:{
+    meta:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fcompany%2Fmeta.png?alt=media&token=884c0e41-b971-40cf-96aa-95363f2191b8",
+    starbucks:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fcompany%2Fstarbucks.png?alt=media&token=a7d89b79-b954-43ff-bf31-526805d916cd",
+    tesla:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fcompany%2Ftesla.png?alt=media&token=5a55e319-45d9-4a8c-b889-805facfb0724",
+    shopify:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fcompany%2Fshopify.png?alt=media&token=01b495b7-a187-4349-ad85-f35565dd9e80"    
+  },
+  carrent:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fcarrent.png?alt=media&token=be86eb79-bd1c-4b52-9baa-80df7cf1a3c4",
+  jobit:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Fjobit.png?alt=media&token=71014b4d-2626-4361-a546-d5d2cd7d3802",
+  tripguide:"https://firebasestorage.googleapis.com/v0/b/portifolio-16fa8.appspot.com/o/PedroCeglia%2Foutros%2Fprojeto-portifolio-UI-%2Ftripguide.png?alt=media&token=8e6df136-882d-4ab3-b0f5-1cc3f12ba8ce"
+}
+
 export const navLinksList = [
-    {path:"/",linkName:"Home"},
-    {path:"/tragetoria",linkName:"Tragetória"},
-    {path:"/info",linkName:"Informações"},
-    {path:"/tecnologias",linkName:"Tecnologias"},
-    {path:"/contato",linkName:"Entre em contato"},
-]
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: assetsImage.web,
+  },
+  {
+    title: "React Native Developer",
+    icon: assetsImage.mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: assetsImage.backend,
+  },
+  {
+    title: "Content Creator",
+    icon: assetsImage.creator,
+  }
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: assetsImage.tech.html,
+  },
+  {
+    name: "CSS 3",
+    icon: assetsImage.tech.css,
+  },
+  {
+    name: "JavaScript",
+    icon: assetsImage.tech.javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: assetsImage.tech.typescript,
+  },
+  {
+    name: "React JS",
+    icon: assetsImage.tech.reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: assetsImage.tech.redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: assetsImage.tech.tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: assetsImage.tech.nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: assetsImage.tech.mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: assetsImage.tech.threejs,
+  },
+  {
+    name: "git",
+    icon: assetsImage.tech.git,
+  },
+  {
+    name: "figma",
+    icon: assetsImage.tech.figma,
+  },
+  {
+    name: "docker",
+    icon: assetsImage.tech.docker,
+  }
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Starbucks",
+    src: assetsImage.company.starbucks,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "Tesla",
+    src: assetsImage.company.tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Shopify",
+    src: assetsImage.company.shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Meta",
+    src: assetsImage.company.meta,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: assetsImage.carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: assetsImage.jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: assetsImage.tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
