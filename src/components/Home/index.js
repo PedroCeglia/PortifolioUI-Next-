ad182b0d80f86447efb064eb1cbbb3c3166128b1
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-10 01:14:56 -0300

    Import & Export "Historico"

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import EstruturaPagina from "../EstruturaPagina";
 import Intro from "./Intro";
 import AboutMe from "./AboutMe";
+import Historico from "./Hisorico";
 
-export {EstruturaPagina, Intro, AboutMe}
+export {EstruturaPagina, Intro, AboutMe, Historico}
