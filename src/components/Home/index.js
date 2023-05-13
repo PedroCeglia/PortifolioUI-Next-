b2aaae1e3cb68f23041b21a2f7327a72ad40a8a1
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-13 00:42:57 -0300

    Create a file to export all files to home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,7 @@
+import EstruturaPagina from "../EstruturaPagina";
+import IntroSection from "./Intro";
+import AboutMeSection from "./AboutMe";
+import HistoricoSection from "./Hisorico";
+import TechSection from "./Tech"
+
+export {EstruturaPagina, IntroSection, AboutMeSection, HistoricoSection, TechSection}
