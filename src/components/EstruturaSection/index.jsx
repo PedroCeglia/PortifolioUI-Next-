6655272cdd4e8871fdb0d4f4bba1e9597ff5cebc
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-13 00:41:51 -0300

    Create EstruturaSection Component

--- src/components/EstruturaSection/index.jsx
+++ src/components/EstruturaSection/index.jsx
@@ -2,4 +2,41 @@ import ChangeSection from "./ChangeSection";
 import ControlSection from "./ControlSection";
 import HeaderSection from "./HeaderSection";
 
-export {ChangeSection, ControlSection, HeaderSection}
+import { EstruturaSectionStyle, EstruturaSectionIntroStyle } from "./style";
+
+
+export default function EstruturaSection({children, headerSectionDates, changeSectionSlug, idSection}){
+    function veryfyIfSectionIsOdd(){
+        if(idSection=="about-me" || idSection=="tech"){
+            return true
+        }
+    }
+    const sectionIsOdd = veryfyIfSectionIsOdd()
+
+    if(headerSectionDates != null){
+        return(
+            <ControlSection idSection={idSection}>
+                <EstruturaSectionStyle isOdd={sectionIsOdd}>
+                    <HeaderSection
+                        preTitle={headerSectionDates.preTitle}
+                        title={headerSectionDates.title}
+                        description={headerSectionDates.description}
+                    />
+                    {children}
+                    <ChangeSection slug={changeSectionSlug}/>
+                </EstruturaSectionStyle>
+            </ControlSection>
+        )
+    } else{
+        return(
+            <ControlSection idSection={idSection}>
+                <EstruturaSectionIntroStyle>
+                    {children}
+                    <ChangeSection slug={changeSectionSlug}/>
+                </EstruturaSectionIntroStyle>
+            </ControlSection> 
+        )
+    }
+
+    
+}
