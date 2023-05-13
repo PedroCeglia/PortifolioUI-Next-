92465f3c09a5d0600b49c7407fd71bf937c0edc0
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-13 00:43:31 -0300

    Change this file to .js

--- src/components/Home/index.jsx
+++
@@ -1,6 +0,0 @@
-import EstruturaPagina from "../EstruturaPagina";
-import IntroSection from "./Intro";
-import AboutMeSection from "./AboutMe";
-import HistoricoSection from "./Hisorico";
-
-export {EstruturaPagina, IntroSection, AboutMeSection, HistoricoSection}
