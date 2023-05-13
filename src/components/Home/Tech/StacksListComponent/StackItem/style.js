cf2fb4e89a8914313fc5eced1bcd186055566575
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-13 00:49:31 -0300

    Create Tech StyledComponents

---
+++ src/components/Home/Tech/StacksListComponent/StackItem/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import style from "@/styles";
+
+export const StackItemStyle = styled.div`
+    width: 100%;
+    height: 100%;
+    max-width: 150px;
+    max-height: 150px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+` 
---
+++ src/components/Home/Tech/StacksListComponent/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import style from "@/styles";
+
+export const StacksListComponentStyle = styled.div`
+    width: 80%;
+    min-width: 280px;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+`
