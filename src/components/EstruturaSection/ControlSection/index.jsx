cec5b2d9de025cfe80c358a98aca0ef8d1aeb84b
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-13 00:45:14 -0300

    Change the Componet props to children props

--- src/components/EstruturaSection/ControlSection/index.jsx
+++ src/components/EstruturaSection/ControlSection/index.jsx
@@ -1,8 +1,7 @@
 import { motion } from "framer-motion";
 import { staggerContainer } from "@/constants/motion";
 
-export default function ControlSection (Component, idName){
-  return function HOC() {
+export default function ControlSection ({idSection,children}){
     return (
       <motion.section
         variants={staggerContainer()}
@@ -11,9 +10,8 @@ export default function ControlSection (Component, idName){
         viewport={{ once: true, amount: 0.25 }}
         style={{width:"100%", height:"100%"}}
       >
-        <div  id={idName}></div>
-        <Component />
+        <div id={idSection}></div>
+        {children}
       </motion.section>
     );
-  };    
-}
+  }    
