b6d2997cca9d6bcce74f06f874766b872752a3dc
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-14 05:19:10 -0300

    Create ContactMeSectionStyle Styled-Component

---
+++ src/components/Home/ContactMe/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContactMeSectionStyle = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 90px 0 40px;
+    position: relative;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
