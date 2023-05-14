b4f78b49c678ce14e9df5bd028bfb799e0b188b4
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-14 05:16:43 -0300

    Import and Export ContactMeSection

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,5 +3,6 @@ import IntroSection from "./Intro";
 import AboutMeSection from "./AboutMe";
 import HistoricoSection from "./Hisorico";
 import TechSection from "./Tech"
+import ContactMeSection from "./ContactMe";
 
-export {EstruturaPagina, IntroSection, AboutMeSection, HistoricoSection, TechSection}
+export {EstruturaPagina, IntroSection, AboutMeSection, HistoricoSection, TechSection, ContactMeSection}
