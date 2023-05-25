4cbe7f947bbc5cec329682d81f94e8c08e64f6de
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-25 16:52:54 -0300

    import StackBall3DComponent as dynamic to rendder in client side

--- src/components/Home/Tech/StacksListComponent/StackItem/index.jsx
+++ src/components/Home/Tech/StacksListComponent/StackItem/index.jsx
@@ -1,10 +1,10 @@
 import { StackItemStyle } from "./style";
-import StacksBallCanvasComponent from "@/components/3D/StacksBallComponent";
+import { StackBall3DComponent } from "@/components/3D";
 
 export default function StackItem({images}){
     return(
         <StackItemStyle>
-            <StacksBallCanvasComponent images={images}/>
+            <StackBall3DComponent images={images}/>
         </StackItemStyle>
     )
 }
