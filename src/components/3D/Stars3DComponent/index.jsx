032da5aebae9c6daa51a6c6568a62fb7c2a4aa29
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-25 16:47:03 -0300

    create Stars3DComponent

---
+++ src/components/3D/Stars3DComponent/index.jsx
@@ -0,0 +1,44 @@
+import {useRef, Suspense, useState} from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Points, PointMaterial, Preload } from "@react-three/drei"
+import * as random  from "maath/random";
+
+
+function StarsPointsComponent(props){
+
+    const pointsRef = useRef();
+    const [ pointsPositions ] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.25 })); 
+    console.log(pointsPositions)   
+
+    useFrame((state, delta) => {
+        pointsRef.current.rotation.x -= delta / 10;
+        pointsRef.current.rotation.y -= delta / 15;
+    })
+
+    return(
+        <group rotation={[0, 0, Math.PI / 4]} >
+            <Points ref={pointsRef}  positions={pointsPositions} stride={3} frustumCulled {...props}>
+                <PointMaterial
+                    transparent
+                    color='#f272c8'
+                    size={0.005}
+                    sizeAttenuation={true}
+                    depthWrite={false}
+                />
+            </Points>
+        </group>
+    )
+}
+
+export default function StarsCanvasComponent(){
+    return(
+        <Canvas camera={{position:[0,0,1]}}>
+            <Suspense fallback={null}>
+                <StarsPointsComponent />
+            </Suspense>
+            <Preload all/>
+        </Canvas>     
+    )
+}
+
+
