8f484d302ab14c800d8e8bdb480053cf9e828ac6
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-05-25 16:50:25 -0300

    Create Stars3DContainer component

---
+++ src/components/Home/ContactMe/StartsContainer/index.jsx
@@ -0,0 +1,10 @@
+import { Stars3DComponent } from "@/components/3D";
+import { StarsContainerStyle } from "./style";
+
+export default function Stars3DContainer(){
+    return(
+        <StarsContainerStyle>
+            <Stars3DComponent/>
+        </StarsContainerStyle>
+    )
+}
